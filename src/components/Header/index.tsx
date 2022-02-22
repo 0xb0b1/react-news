5ee00bd35023531650fe1f20515659b1f782f702
Paulo Vicente <vcente82@gmail.com>
2022-02-21 22:11:24 -0300

    basic header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,16 +2,21 @@
 import Image from "next/image";
 import styles from "./styles.module.scss";
 
+interface IProps {
+  valor: string;
+}
 export const Header = () => {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <img
+          className={true ? "name" : ""}
+          src="/images/logo.svg"
+          alt="ig.news"
+        />
 
         <nav>
-          <a className={styles.active} href="">
-            Home
-          </a>
+          <a href="">Home</a>
           <a href="">Posts</a>
         </nav>
       </div>
---
+++ src/components/SignInButton.tsx
@@ -0,0 +1,7 @@
+export const SignInButton = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
