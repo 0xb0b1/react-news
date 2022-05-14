7e1ab9b80953e709206f511e29386a10bb1b893f
Paulo Vicente <vcente82@gmail.com>
2022-05-14 11:12:32 -0300

    ...

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,11 @@
 import Image from "next/image";
 import styles from "./styles.module.scss";
 
-interface IProps {
-  valor: string;
-}
 export const Header = () => {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img
-          className={true ? "name" : ""}
-          src="/images/logo.svg"
-          alt="ig.news"
-        />
+        <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
           <a href="">Home</a>
