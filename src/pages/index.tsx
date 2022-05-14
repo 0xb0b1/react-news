4880efe254ed51636afa25fdd85cc09a8ad6e579
Paulo Vicente <vcente82@gmail.com>
2022-05-14 12:38:59 -0300

    using server side rendering to retrive product price from stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
-import type { NextPage } from "next";
+/* eslint-disable @next/next/no-img-element */
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import { Header } from "../components/Header";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
 
-export const Home: NextPage = () => {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export const Home = ({ product }: HomeProps) => {
+  // console.log(props);
+
   return (
     <>
       <Head>
@@ -20,15 +31,33 @@ export const Home: NextPage = () => {
           </h1>
           <p>
             Get access to all the publication <br />
-            <span>for $9.99 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1KzM6mJLsp0sEVUkFmFulRb1");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
 };
 
 export default Home;
